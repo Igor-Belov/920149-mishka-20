dbe7c051fbb5506c9e2fe4c3a078923339220894
Igor-Belov <Belovis84@gmail.com>
2020-09-03 12:27:32 +0300

    подлатал JS

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,12 +1,18 @@
 let button = document.querySelector(".js-button-toggle-nav");
 let menu = document.querySelector(".js-site-list");
+let cart = document.querySelector(".main-navigation__cart");
+let search = document.querySelector(".main-navigation__search");
 
 button.classList.add ("button-toggle-nav--on");
 menu.classList.add ("main-navigation__list-site--off");
+cart.classList.add ("main-navigation__cart--off");
+search.classList.add ("main-navigation__search--off");
 
 button.addEventListener("click", function () {
   button.classList.toggle ("button-toggle-nav--close");
   menu.classList.toggle ("main-navigation__list-site--off");
+  cart.classList.toggle ("main-navigation__cart--off");
+  search.classList.toggle ("main-navigation__search--off");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -14,6 +20,8 @@ window.addEventListener("keydown", function (evt) {
     if (!menu.classList.contains("main-navigation__list-site--off")) {
       evt.preventDefault();
       menu.classList.add("main-navigation__list-site--off");
+      cart.classList.add ("main-navigation__cart--off");
+      search.classList.add ("main-navigation__search--off");
     }
   }
 });
